8294b4c256f74913456976fabbd3cc9af649b2f8
unknown <mujtaba.syed19@gmail.com>
2020-12-24 12:36:30 -0600

    7.Reverse Integer

---
+++ src/Solution7.java
@@ -0,0 +1,28 @@
+//O(n) 
+class Solution7 {
+    public int reverse(int x) {
+        boolean negative = false;
+        String reverse = "";
+        if (x < 0) {
+            x = x * (-1);
+            negative = true;
+        }
+        while(x != 0) {
+            int y = x % 10;
+            x = x / 10;
+            reverse = reverse + Integer.toString(y);
+            //reverse = reverse + y;
+        }
+        try {
+            int result = Integer.parseInt(reverse);
+            if (negative) {
+                result = result *(-1);
+            }
+            return result;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
+
+
