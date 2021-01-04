9d60bd22633316b1cde9a43e137276a163a8a797
unknown <mujtaba.syed19@gmail.com>
2021-01-04 11:03:37 -0600

    20.Valid Paranthesis

---
+++ src/Solution20.java
@@ -0,0 +1,56 @@
+//O (n)
+class Solution20 {
+    public boolean isValid(String s) {
+        Stack<Character> characters = new Stack<>();
+      
+        String dup = s; 
+        char store = ' ';
+       
+            for (int i = 0;i < s.length();i++) {
+                char brackets = s.charAt(i);
+            
+                if (brackets == ')') {
+                    if (!characters.empty()) {
+                        store = characters.pop();
+                        if (store != '(') {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                else if (brackets == ']') {
+                    if (!characters.empty()) {
+                        store = characters.pop();
+                        if (store != '[') {
+                             return false;
+                        } 
+                    } else {
+                        return false;
+                    }
+                }
+                else if(brackets == '}') {
+                    if(!characters.empty()) {
+                        store = characters.pop();
+                        if (store != '{') {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                else {
+                    characters.push(brackets);
+                }
+            
+            }
+        
+        if(!characters.empty()) {
+            return false; 
+        } 
+        
+     return true;
+       
+    }
+    
+}
